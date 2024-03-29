f3779936ee288b94dbb33277372b1e7e2853282f
BOYANWUISGREATATCODING <99688312+BOYANWUISGREATATCODING@users.noreply.github.com>
2024-03-29 00:44:02 -0400

    vite config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig( config: {
   plugins: [react()],
-  base: '/vite-react-m27-SI-579/',
+  base: '/vite-react-m27-SI-579-/',
 })
